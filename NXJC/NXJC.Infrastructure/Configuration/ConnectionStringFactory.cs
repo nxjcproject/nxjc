b3def35b95e135d170f8e01750817bf1cb70f0a1
zcsminifast <chongshanzhang1988@163.com>
2014-09-16 11:03:23 +0800

    ConnectionStringFactory最终版

--- NXJC/NXJC.Infrastructure/Configuration/ConnectionStringFactory.cs
+++ NXJC/NXJC.Infrastructure/Configuration/ConnectionStringFactory.cs
@@ -1,6 +1,5 @@
 ﻿using SqlServerDataAdapter;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -11,29 +10,17 @@ namespace NXJC.Infrastructure.Configuration
     public static class ConnectionStringFactory
     {
         /// <summary>
-        /// 以生产线ID为键的数据库字典
+        /// DatabaseModel仓库
         /// </summary>
-        private static readonly Dictionary<int, IList<DatabaseModel>> ProductLineToDatabase = new Dictionary<int,IList<DatabaseModel>>();
+        private static readonly IList<DatabaseModel> databaseBox = new List<DatabaseModel>();
         /// <summary>
-        /// 以DCS系统ID为键的数据库字典
-        /// </summary>
-        private static readonly Dictionary<int, IList<DatabaseModel>> DCSSystemToDatabase = new Dictionary<int, IList<DatabaseModel>>();
-        /// <summary>
-        /// 以分厂ID为键的数据库字典
-        /// </summary>
-        private static readonly Dictionary<int, IList<DatabaseModel>> FactoryToDatabase = new Dictionary<int, IList<DatabaseModel>>();
-        /// <summary>
-        /// 以分公司ID为键的数据库字典
-        /// </summary>
-        private static readonly Dictionary<int, IList<DatabaseModel>> CompanyToDatabase = new Dictionary<int, IList<DatabaseModel>>();
-        /// <summary>
-        /// 数据库访问类
+        /// 数据库访问工厂
         /// </summary>
         private static ISqlServerDataFactory dataFactory;
         /// <summary>
         /// 集团管理数据库连接字符串
         /// </summary>
-        public static string NXJCConnectionString { get; set; }
+        public static string NXJCConnectionString { get; private set; }
 
         static ConnectionStringFactory()
         {
@@ -42,11 +29,11 @@ namespace NXJC.Infrastructure.Configuration
             Initialize();
         }
         /// <summary>
-        /// 初始化数据库字典
+        /// 初始化DatabaseModel仓库
         /// </summary>
-        public static void Initialize()
+        private static void Initialize()
         {
-            ProductLineToDatabase.Clear();
+            databaseBox.Clear();
             Query query = new Query("DatabaseContrast");
             DataTable table = dataFactory.Query(query);
 
@@ -54,151 +41,110 @@ namespace NXJC.Infrastructure.Configuration
             {
                 DatabaseModel model = new DatabaseModel
                 {
+                    DatabaseContrastID = int.Parse(row["DatabaseContrastID"].ToString()),
+                    CompanyID = int.Parse(row["CompanyID"].ToString()),
+                    DCSSystemID = int.Parse(row["DCSSystemID"].ToString()),
+                    FactoryID = int.Parse(row["FactoryID"].ToString()),
+                    ProductLineID = int.Parse(row["ProductLineID"].ToString()),
+                    Type = int.Parse(row["Type"].ToString()),
+
                     DatabaseName = row["DatabaseName"].ToString().Trim(),
                     IPAddress = row["IPAddress"].ToString().Trim(),
                     UserID = row["UserID"].ToString().Trim(),
-                    Password = row["Password"].ToString().Trim(),
-                    Type = int.Parse(row["Type"].ToString())
+                    Password = row["Password"].ToString().Trim()
                 };
 
-                //初始化以生产线ID为键的数据库字典
-                {
-                    int key = int.Parse(row["ProductLineID"].ToString());
-                    if (!ProductLineToDatabase.Keys.Contains(key))                //判断键是不是已经存在于字典中，如果没有则添加该键值对
-                    {
-                        ProductLineToDatabase.Add(key, new List<DatabaseModel>());
-                    }
-               
-                    if (!ProductLineToDatabase[key].Contains(model))
-                    {
-                        ProductLineToDatabase[key].Add(model);
-                    }
-                }
-                //初始化以DCS系统ID为键的数据库字典
-                {
-                    int key = int.Parse(row["DCSSystemID"].ToString());
-                    if (!DCSSystemToDatabase.Keys.Contains(key))
-                    {
-                        DCSSystemToDatabase.Add(key, new List<DatabaseModel>());
-                    }
-
-                    if (!DCSSystemToDatabase[key].Contains(model))
-                    {
-                        DCSSystemToDatabase[key].Add(model);
-                    }
-                }
-                //初始化以分厂ID为键的数据库字典
-                {
-                    int key = int.Parse(row["FactoryID"].ToString());
-                    if (!FactoryToDatabase.Keys.Contains(key))
-                    {
-                        FactoryToDatabase.Add(key, new List<DatabaseModel>());
-                    }
-
-                    if (!FactoryToDatabase[key].Contains(model))
-                    {
-                        FactoryToDatabase[key].Add(model);
-                    }
-                }
-                //初始化以分公司ID为键的数据库字典
-                {
-                    int key = int.Parse(row["CompanyID"].ToString());
-                    if (!CompanyToDatabase.Keys.Contains(key))
-                    {
-                        CompanyToDatabase.Add(key, new List<DatabaseModel>());
-                    }
-
-                    if (!CompanyToDatabase[key].Contains(model))
-                    {
-                        CompanyToDatabase[key].Add(model);
-                    }
-                }
+                databaseBox.Add(model);
             }
         }
-
-        public static string GetNXJCConnectionString()
-        {
-            return ApplicationSettingsFactory.GetApplicationSettings().NXJCConnectionString;
-        }
         /// <summary>
-        /// 根据生产线ID获得连接字符串集合
-        /// </summary>
-        /// <param name="productLineID"></param>
-        /// <param name="type"></param>
-        /// <returns></returns>
-        public static ArrayList GetConnectionStringsByProductLineID(int productLineID, DatabaseType type)
-        {
-            ArrayList result = new ArrayList();
-            foreach (var item in ProductLineToDatabase[productLineID])
-            {
-                if (item.Type == (int)type)
-                {
-                    result.Add(TranslateToConnectionString(item));
-                }
-            }
-            return result;
-        }
-        /// <summary>
-        /// 根据生产线ID获得连接字符串
+        /// 通过生产线ID获得连接字符串
         /// </summary>
         /// <param name="productLineID"></param>
         /// <param name="type"></param>
         /// <returns></returns>
         public static string GetConnectionStringByProductLineID(int productLineID, DatabaseType type)
         {
-            DatabaseModel model = ProductLineToDatabase[productLineID].FirstOrDefault();
-            string result = TranslateToConnectionString(model);
-            return result;
+            DatabaseModel model;
+
+            if (databaseBox.Where(d => d.ProductLineID == productLineID && d.Type == (int)type).Count() > 1)
+                throw new ApplicationException("指定数据库不唯一");
+
+            model = databaseBox.FirstOrDefault(d => d.ProductLineID == productLineID && d.Type == (int)type);
+            if (model != null)
+            {
+                return TranslateToConnectionString(model);
+            }
+            else
+            {
+                throw new ApplicationException("未找到指定数据库");
+            }
         }
         /// <summary>
-        /// 根据DCS系统ID获得连接字符串
+        /// 通过DCS系统ID获得连接字符串
         /// </summary>
         /// <param name="dcsSystemID"></param>
         /// <param name="type"></param>
         /// <returns></returns>
-        public static string GetConnectionStringByDCSSystemID(int dcsSystemID, DatabaseType type)
+        public static string GetConnectionStringByDSCSystemID(int dcsSystemID, DatabaseType type)
         {
-            DatabaseModel model = DCSSystemToDatabase[dcsSystemID].FirstOrDefault();
-            string result = TranslateToConnectionString(model);
-            return "";
+            DatabaseModel model;
+
+            if (databaseBox.Where(d => d.DCSSystemID == dcsSystemID && d.Type == (int)type).Count() > 1)
+                throw new ApplicationException("指定数据库不唯一");
+
+            model = databaseBox.FirstOrDefault(d => d.DCSSystemID == dcsSystemID && d.Type == (int)type);
+            if (model != null)
+            {
+                return TranslateToConnectionString(model);
+            }
+            else
+            {
+                throw new ApplicationException("未找到指定数据库");
+            }
         }
         /// <summary>
-        /// 根据分厂ID获得连接字符串集合
+        /// 根据生产线ID获得连接字符串集合
         /// </summary>
         /// <param name="factoryID"></param>
         /// <param name="type"></param>
         /// <returns></returns>
-        public static ArrayList GetConnectionStringByFactoryID(int factoryID, DatabaseType type)
+        public static string[] GetConnectionStringByFactoryID(int factoryID, DatabaseType type)
         {
-            ArrayList result = new ArrayList();
-            foreach (var item in FactoryToDatabase[factoryID])
+            IList<string> results = new List<string>();
+            List<DatabaseModel> models = databaseBox.Where(d => d.FactoryID == factoryID && d.Type == (int)type).ToList();
+            if (models.Count() == 0)
             {
-                if (item.Type == (int)type)
-                {
-                    result.Add(TranslateToConnectionString(item));
-                }
+                throw new ApplicationException("未找到指定数据库");
             }
-            return result;
+
+            foreach (var item in models)
+            {
+                results.Add(TranslateToConnectionString(item));
+            }
+            return results.ToArray();
         }
         /// <summary>
-        /// 根据分公司ID获得连接字符串集合
+        /// 根据分厂ID获得连接字符串集合
         /// </summary>
         /// <param name="companyID"></param>
         /// <param name="type"></param>
         /// <returns></returns>
-        public static ArrayList GetConnectionStringByCompanyID(int companyID, DatabaseType type)
+        public static string[] GetConnectionStringByCompanyID(int companyID, DatabaseType type)
         {
-            ArrayList result = new ArrayList();
-            foreach (var item in CompanyToDatabase[companyID])
+            IList<string> results = new List<string>();
+            List<DatabaseModel> models = databaseBox.Where(d => d.CompanyID == companyID && d.Type == (int)type).ToList();
+            if (models.Count() == 0)
             {
-                if (item.Type == (int)type)
-                {
-                    result.Add(TranslateToConnectionString(item));
-                }
+                throw new ApplicationException("未找到指定数据库");
             }
-            return result;
-        }
 
+            foreach (var item in models)
+            {
+                results.Add(TranslateToConnectionString(item));
+            }
+            return results.ToArray();
+        }
         /// <summary>
         /// 将DatabaseModel翻译成连接字符串
         /// </summary>
--- NXJC/NXJC.Infrastructure/Configuration/DatabaseModel.cs
+++ NXJC/NXJC.Infrastructure/Configuration/DatabaseModel.cs
@@ -7,16 +7,21 @@ namespace NXJC.Infrastructure.Configuration
 {
     public class DatabaseModel
     {
+        public int DatabaseContrastID { get; set; }
+        public int CompanyID { get; set; }
+        public int FactoryID { get; set; }
+        public int DCSSystemID { get; set; }
+        public int ProductLineID { get; set; }
+        public int Type { get; set; }
+
         public string DatabaseName { get; set; }
         public string IPAddress { get; set; }
         public string UserID { get; set; }
         public string Password { get; set; }
-        public int Type { get; set; }
 
         public override bool Equals(object obj)
         {
-            if (((DatabaseModel)obj).DatabaseName == this.DatabaseName && ((DatabaseModel)obj).IPAddress == this.IPAddress &&
-                ((DatabaseModel)obj).UserID == this.UserID && ((DatabaseModel)obj).Password == this.Password && ((DatabaseModel)obj).Type == this.Type)
+            if (((DatabaseModel)obj).DatabaseContrastID == this.DatabaseContrastID)
             {
                 return true;
             }
